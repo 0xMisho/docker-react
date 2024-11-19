8612d04ca42057c92e938ff69dd1a7ece8cc5f46
MihailGenchev <mihailgenchev@MihailGenchevs-MacBook-Pro.local>
2024-11-20 00:21:16 +0200

    added new text in home page

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          baaaa sum lud!
+          Obicham Simona!
         </p>
         <a
           className="App-link"
